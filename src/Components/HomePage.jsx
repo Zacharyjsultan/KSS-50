34baabcd677cd0302437f4c9f1883c8885156a14
Zach <Zach@Zanzibars-Computer.local>
2023-07-05 10:59:56 -0700

    all links working - time for css

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <div>
+        <h1>HomePage</h1>
+        <Link to="/" className="button-1">
+          Go to HomePage
+        </Link>
+        <Link to="/contact" className="button-2">
+          Contact Me
+        </Link>
+      </div>
+    </div>
+  );
 }
