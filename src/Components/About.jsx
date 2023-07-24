1d6d76b3c863fe5691c818537cda868549a5128d
Zach <Zach@Zanzibars-Computer.local>
2023-07-23 22:17:19 -0700

    form work on contact / css

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./About.css";
 import { Button } from "@mui/material";
+import Footer from "./Header&Footer/Footer";
+import Header from "./Header&Footer/Header";
 
 export default function About() {
   return (
     <div className="about-background">
+      <Header />
       <div className="container">
         <div className="entry-button">
-          <Button href="/" className="button">
-            Keely Shea
-          </Button>
+          <Button href="/" className="button"></Button>
+        </div>
+        <div>
+          <Footer />
         </div>
       </div>
     </div>
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -7,62 +7,65 @@ export default function Contact() {
     <div>
       <Header />
       <div className="contact-page">
-        <div>
-          <form
-            action="https://public.herotofu.com/v1/b45438b0-2594-11ee-8058-515da3888232"
-            method="post"
-            accept-charset="UTF-8"
-          >
-            <div>
-              <label for="name">Name</label>
-              <input
-                name="Name"
-                id="name"
-                type="text"
-                placeholder="Your Name"
-                required
-              />
-            </div>
-            <div>
-              <label for="name"> Email</label>
-              <input
-                name="Email"
-                id="email"
-                type="text"
-                placeholder="YourEmail@Gmail.com"
-                required
-              />
-            </div>
-            <div>
-              <label for="email">Message</label>
-              <input
-                name="Message"
-                id="message"
-                type="text"
-                placeholder="How can I help?"
-                required
-              />
-            </div>
-            <div>
-              <input type="submit" value="Reach Out" />
-              <div
-                style={{
-                  textIndent: "-99999px",
-                  whiteSpace: "nowrap",
-                  overflow: "hidden",
-                  position: "absolute",
-                }}
-                aria-hidden="true"
-              >
+        <div className="contact-title">
+          <h1>Contact Me</h1>
+          <div className="contact-form-container">
+            <form
+              action="https://public.herotofu.com/v1/b45438b0-2594-11ee-8058-515da3888232"
+              method="post"
+              accept-charset="UTF-8"
+            >
+              <div>
+                <label htmlFor="name">Name</label>
                 <input
+                  name="Name"
+                  id="name"
                   type="text"
-                  name="_gotcha"
-                  tabindex="-1"
-                  autocomplete="off"
+                  placeholder="Your Name"
+                  required
                 />
               </div>
-            </div>
-          </form>
+              <div>
+                <label htmlFor="email">Email</label>
+                <input
+                  name="Email"
+                  id="email"
+                  type="text"
+                  placeholder="YourEmail@Gmail.com"
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="message">Message</label>
+                <textarea
+                  name="Message"
+                  id="message"
+                  placeholder="How can I help?"
+                  required
+                ></textarea>
+              </div>
+              <div>
+                <input type="submit" value="Reach Out" />
+                <div
+                  style={{
+                    textIndent: "-99999px",
+                    whiteSpace: "nowrap",
+                    overflow: "hidden",
+                    position: "absolute",
+                  }}
+                  aria-hidden="true"
+                >
+                  <input
+                    type="text"
+                    name="_gotcha"
+                    tabIndex="-1"
+                    autoComplete="off"
+                  />
+                  <div></div>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
