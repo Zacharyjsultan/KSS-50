92ceeda3a820d0c00a6228d18d598eda11de1e88
Zach <Zach@Zanzibars-Computer.local>
2023-10-05 20:12:04 -0700

    underconstruction page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./Components/About";
-import HomePage from "./Components/HomePage";
+// import HomePage from "./Components/HomePage";
 import Contact from "./Components/Contact";
+import UnderConstruction from "./Components/components/UnderConstruction";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}{" "}
+        {/* change route path
+        when done */}
+        <Route path="/" element={<UnderConstruction />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
---
+++ src/Components/components/UnderConstruction.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./UnderConstruction.css";
+
+function UnderConstruction() {
+  return (
+    <div>
+      <div className="uc-container">
+        <h1>Keely Shea </h1>
+        <h3>Birth Doula & Reiki Master</h3>
+        <p>We're working on something awesome. Please check back soon!</p>
+        <h3>Inquire Below!</h3>
+        <a className="emailme" href="mailto:sheabirthandhealing@gmail.com">
+          Email Me
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default UnderConstruction;
