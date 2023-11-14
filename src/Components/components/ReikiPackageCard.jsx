5ee8eedf7493094fb52c90a59591b9b2cd1ec160
Zach <Zach@Zanzibars-Computer.local>
2023-11-14 15:18:42 -0800

    mapping through features of card

--- src/Components/components/ReikiPackageCard.jsx
+++ src/Components/components/ReikiPackageCard.jsx
@@ -4,9 +4,13 @@ import "./ReikiPackageCard.css";
 const ReikiPackageCard = () => {
   const reikiPackage = {
     title: "Reiki",
+    description:
+      "Experience the healing power of Reiki with our tailored sessions.",
     prices: [
       {
         price: "$85.00/hour",
+        description:
+          "Indulge in a relaxing experience with a sound bath, oil diffusion, and personalized sessions.",
         features: ["Sound bath", "Oil Diffusion", "Tailored sessions"],
       },
     ],
@@ -15,9 +19,11 @@ const ReikiPackageCard = () => {
   return (
     <div className="package">
       <h2 className="package-title">{reikiPackage.title}</h2>
+      <p className="package-description">{reikiPackage.description}</p>
       {reikiPackage.prices.map((price, priceIndex) => (
         <div className="package-price" key={priceIndex}>
           {price.price}
+          <p className="package-description">{price.description}</p>
           <ul className="package-features">
             {price.features.map((feature, featureIndex) => (
               <li key={featureIndex}>{feature}</li>
