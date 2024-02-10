e98984a87213b65c3b6b79fd053fb2a46262d617
Zach <Zach@Zanzibars-Computer.local>
2024-02-10 13:04:08 -0800

    trying to fix for mobile

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./Components/About";
-// import HomePage from "./Components/HomePage";
+import HomePage from "./Components/HomePage";
 import Contact from "./Components/Contact";
-import UnderConstruction from "./Components/components/UnderConstruction";
+// import UnderConstruction from "./Components/components/UnderConstruction";
 import Testimonals from "./Components/Testimonals";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={<HomePage />} /> */}{" "}
+        <Route path="/" element={<HomePage />} />
         {/* change route path
         when done */}
-        <Route path="/" element={<UnderConstruction />} />
+        {/* <Route path="/" element={<UnderConstruction />} /> */}
         <Route path="/about" element={<About />} />
         <Route path="/testimonials" element={<Testimonals />} />
         <Route path="/contact" element={<Contact />} />
--- src/Components/Testimonals.jsx
+++ src/Components/Testimonals.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Testimonals() {
-  return <div>Testimonals</div>;
+  return (
+    <div>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+    </div>
+  );
 }
