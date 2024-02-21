92fd426ab5d73a188cc07aec577e62a6b45c3d9a
Zach <Zach@Zanzibars-Computer.local>
2024-02-21 09:56:21 -0800

    reviews

--- src/Components/Testimonals.jsx
+++ src/Components/Testimonals.jsx
@@ -6,6 +6,24 @@ export default function Testimonals() {
     <div>
       <div>
         <Link to="/">Home</Link>
+        <div>
+          Keely, You are wonderful at what you do. The reiki session I had with
+          you was revealing and healing. I hope to book a session with you again
+          soon! -Lori
+          <div>
+            As a mom who needs a space to just be and allow relaxation and
+            restoration set in, Keely provides a wonderful space. She also
+            helped clear a few blocks I had. I especially enjoyed the presence
+            Keely brings, the ambiance and details to the space, and the flow of
+            the Reiki session. It was wonderful, and I will book again soon.
+            -Brandy
+          </div>
+          <div>
+            1 hour with Keely and it was like years of stuckness were gently and
+            lovingly released. She has a true gift of returning flow and
+            connection from within. Thank you, Keely! -Erica
+          </div>
+        </div>
       </div>
     </div>
   );
